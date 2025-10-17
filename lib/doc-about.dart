df71643ba83961cd19f208825ff0ede9a66170bc
rafif-al <rafifalfauan@gmail.com>
2025-10-17 09:21:34 +0700

    belajar flutter

---
+++ lib/doc-about.dart
@@ -0,0 +1,39 @@
+/** PENJELASAN  */
+
+// ignore: dangling_library_doc_comments
+/**
+ * Struktur Flutter terdiri dari
+ * 1. .dart tool
+ * 2. .idea
+ * 3. android
+ *  => untuk konfigurasi android serta
+ *  => libary (native)
+ * 4. bulid
+ *  => hasil compile aplikasi
+ * 5. ios
+ *  => untuk konfigurasi ios serta
+ *  => libary (native)
+ * 6. lib
+ *  => tempat file dart berada yang nantinya
+ *  => sering di gunakan
+ * 7. linux
+ *  => untuk konfigurasi linux
+ * 8. macos
+ *  => untuk konfigurasi macos
+ * 9. test
+ *  => temapt file untuk testing aplikasi
+ * 10. web
+ *  => untuk konfigurasi web
+ * 11. windows
+ *  => untuk konfigurasi windows
+ * 12. puspec.yaml
+ *  => tempat konfigurasi aplikasi
+ *  => menambahkan library
+ *  => menambahkan asset 
+ * 
+ * di terminal
+ * 
+ * kegunaan perintah "flutter pub get"
+ * => untuk menyingkronkan libary yang ada di puspec.yaml
+ * 
+ */
--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter UHUY'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
